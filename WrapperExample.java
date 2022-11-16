fdc2e1f236f93d65132c1031a07dc04e2bd2ee4b
rsveinson <rsveinson@mts.net>
2022-11-15 19:16:34 -0600

    added some comment to wrapper examples added some array list stuff to the wrapper example class

--- WrapperExample.java
+++ WrapperExample.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 /** 
  * ACS-1903 Assignment X Question Y
  * @author 
@@ -7,7 +9,12 @@ import java.util.Scanner;
 public class WrapperExample{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random rnd = new Random();
+        
+        ArrayList<Integer> list = new ArrayList<>();
         Integer n = new Integer(5);
+        
+        // auto-un-boxing
         int m = n;
         
         System.out.println("n " + n);
@@ -17,9 +24,25 @@ public class WrapperExample{
         System.out.println("n " + n);
         System.out.println("m " + m);
         
+        // auto-boxing
         n = m;
         System.out.println("n " + n);
-        System.out.println("m " + m);        
+        System.out.println("m " + m); 
+        
+        // now some ArrayList Wrapper class stuff
+        list.add(5);        // auto boxing
+        m = list.get(0);    // auto unboxing
+        list.add(rnd.nextInt(10) + 1);  // auto boxing
+        
+        for(int i = 0; i < 5; i++){
+            list.add(rnd.nextInt(11) + 100);
+        }// end for
+        
+        for(Integer i : list){
+            System.out.println(i);
+        }// end for each
+        
+        
 
         System.out.println("end of program");
     }
